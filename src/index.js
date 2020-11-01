575b566c35b1a4517d3d1936de29424dd0d445eb
Rebeca <rebecafguilherme@gmail.com>
2020-11-01 16:52:33 -0300

    botaofuncional

--- src/index.js
+++ src/index.js
@@ -2,23 +2,44 @@
 
 //console.log(cipher);
 
-let entradaTexto = document.getElementById("textoEntrada");
-
-let textoMaiusculo = entradaTexto.toUpperCase();
-
-let tamTexto = textoMaiusculo.lenght();
 
 
 
 
 
-const tamAlfabeto = 26; 
+const botaoCifrar = document.getElementById("botaoCifrar");
+botaoCifrar.addEventListener("click", capturarTexto)
 
-let offset = document.getElementById("valorOffset");
+function capturarTexto(params) {
+   let entradaTexto = document.getElementById("textoEntrada").value; 
+   let entradaMaiuscula = entradaTexto.toUpperCase();
+   document.getElementById("textoSaida").innerHTML= entradaMaiuscula;
+   console.log(entradaMaiuscula);
+}
+
+const botaoDecifrar = document.getElementById("botaoDecifrar");
+botaoDecifrar.addEventListener("click", devolverTexto)
+
+function devolverTexto(params) {
+   let entradaTexto = document.getElementById("textoEntrada").value; 
+   let entradaMaiuscula = entradaTexto.toUpperCase();
+   document.getElementById("textoSaida").innerHTML= entradaMaiuscula;
+   console.log(entradaMaiuscula);
+}
+
+//let tamTexto = textoMaiusculo.lenght();
 
 
-let textoCifradoCodigoASC = ((Number(textoCodigoASC) + Number(offset)) % tamAlfabeto) + Number(textoCodigoASC);
 
-let saidaTexto = String.fromCharCode(textoCifradoCodigoASC);
 
-saidaTexto.innerHTML 
+
+//const tamAlfabeto = 26; 
+
+//let offset = document.getElementById("valorOffset");
+
+
+//let textoCifradoCodigoASC = ((Number(textoCodigoASC) + Number(offset)) % tamAlfabeto) + Number(textoCodigoASC);
+
+//let saidaTexto = String.fromCharCode(textoCifradoCodigoASC);
+
+//saidaTexto.innerHTML -
